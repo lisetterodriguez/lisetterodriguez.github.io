41946a5eb498c47cdaba6158fc46eea0c7cf1df1
Lisette Rodriguez <lisettequennie@yahoo.com>
2023-10-26 20:50:33 +0900

    Create Scourgify file

---
+++ scourgify.py
@@ -0,0 +1,25 @@
+import sys
+
+def main():
+    if len(sys.argv) < 3:
+        print("Too few command-line arguments")
+        sys.exit(1)
+    elif len(sys.argv) > 3:
+        print("Too many command-line arguments")
+        sys.exit(1)
+    else:
+        try:
+            input_file = sys.argv[1]
+
+            with open (input_file, "r") as file:
+                lines = file.readlines()
+
+            selected_lines = [line for line in lines 
+                            if not line.strip().startswith("#") and line.strip() != '']
+            line_count = len(selected_lines)
+            print(line_count)
+        except FileNotFoundError:
+            print("File does not exist")
+if __name__ == "__main__":
+    main()
+######-----------------------END OF CODE------------------------######
